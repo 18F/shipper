21635affeace2feceb9086dee3c94d2b0116b6db
Diego Lapiduz <diego.lapiduz@gsa.gov>
2014-11-19 18:17:37 -0800

    Bump version

--- shipper.go
+++ shipper.go
@@ -100,7 +100,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Jack the shipper"
 	app.Usage = "Continuous deployment made easy and secure"
-	app.Version = "0.0.2"
+	app.Version = "0.1.0"
 
 	if err := checkConfig(); err != nil {
 		log.Println(err)
